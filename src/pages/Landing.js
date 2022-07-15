771c5ec6443c69f88f6112d57b5fb9ad054936d2
Shaw Lu <shawlu95@gmail.com>
2022-07-15 01:38:18 -0700

    Adding components to Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,28 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobster logo' className='logo' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+            iusto, cum, culpa, eum nihil autem eos minus est enim nemo
+            voluptates vitae iste nostrum laboriosam repudiandae facere cumque
+            deserunt vel.
+          </p>
+          <button className='btn btn-hero'>Login / Register</button>
+        </div>
+        <img src={main} alt='job hubt' className='img main-img' />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
