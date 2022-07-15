5ed35b589bb5dfa67d76878e4e2fa1157e66a410
Shaw Lu <shawlu95@gmail.com>
2022-07-15 02:12:23 -0700

    Setup react-router-dom@6

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -19,7 +20,9 @@ const Landing = () => {
             voluptates vitae iste nostrum laboriosam repudiandae facere cumque
             deserunt vel.
           </p>
-          <button className='btn btn-hero'>Login / Register</button>
+          <Link to='/register' className='btn btn-hero'>
+            Login / Register
+          </Link>
         </div>
         <img src={main} alt='job hubt' className='img main-img' />
       </div>
