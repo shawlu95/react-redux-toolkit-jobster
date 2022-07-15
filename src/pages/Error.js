cc3030e68312f341f7b93c0906c750c96630389f
Shaw Lu <shawlu95@gmail.com>
2022-07-15 02:16:43 -0700

    Setup 404 not-found page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
 import React from 'react';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>Page Not Found</h3>
+        <p>We can't find the page you're looking for</p>
+        <Link to='/'>Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
