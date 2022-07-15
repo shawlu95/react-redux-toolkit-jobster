59757082e17eb65e214678c1cf79de2f6d5a2304
Shaw Lu <shawlu95@gmail.com>
2022-07-15 16:37:20 -0700

    Store user state in localStorage

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import customFetch from '../../utils/axios';
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+} from '../../utils/localStorage';
 
 const initialState = {
   isLoading: false,
-  user: null,
+  user: getUserFromLocalStorage(),
 };
 
 export const registerUser = createAsyncThunk(
@@ -42,6 +46,7 @@ const userSlice = createSlice({
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
+      addUserToLocalStorage(user);
       toast.success(`Hello, ${user.name}`);
     },
     [registerUser.rejected]: (state, { payload }) => {
@@ -55,6 +60,7 @@ const userSlice = createSlice({
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
+      addUserToLocalStorage(user);
       toast.success(`Welcome back, ${user.name}`);
     },
     [loginUser.rejected]: (state, { payload }) => {
