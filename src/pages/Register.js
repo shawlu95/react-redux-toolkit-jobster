4799fa7653cd6c5ad4876814f7ed74e6f519b94a
Shaw Lu <shawlu95@gmail.com>
2022-07-15 12:51:25 -0700

    Toggle register and login form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -22,17 +22,24 @@ const Register = () => {
     e.preventDefault();
     console.log(e.target);
   };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
-        <FormRow
-          type='text'
-          name='name'
-          value={values.name}
-          handleChange={handleChange}
-        />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
         <FormRow
           type='email'
           name='email'
@@ -48,6 +55,12 @@ const Register = () => {
         <button type='submit' className='btn btn-block'>
           submit
         </button>
+        <p>
+          {values.isMember ? 'Not a member?' : 'Already a member?'}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
