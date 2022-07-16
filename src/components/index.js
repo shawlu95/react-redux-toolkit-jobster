1edeb5987b7865c5b37d3002a0e8cb5688ea4875
Shaw Lu <shawlu95@gmail.com>
2022-07-16 09:45:50 -0700

    Set up stats page structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,25 @@
 import BigSidebar from './BigSidebar';
+import ChartsContainer from './ChartsContainer';
 import FormRow from './FormRow';
 import FormRowSelect from './FormRowSelect';
 import JobsContainer from './JobsContainer';
+import Loading from './Loading';
 import Logo from './Logo';
 import Navbar from './Navbar';
 import SearchContainer from './SearchContainer';
 import SmallSidebar from './SmallSidebar';
+import StatsContainer from './StatsContainer';
 
 export {
   BigSidebar,
+  ChartsContainer,
   FormRow,
   FormRowSelect,
   JobsContainer,
+  Loading,
   Logo,
   Navbar,
   SearchContainer,
   SmallSidebar,
+  StatsContainer,
 };
