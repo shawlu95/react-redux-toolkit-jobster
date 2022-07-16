d6b15aa6d74e365935fc0c748ee60eb1e41f49c9
Shaw Lu <shawlu95@gmail.com>
2022-07-16 01:30:09 -0700

    Set job details when edit

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -56,6 +56,9 @@ const jobSlice = createSlice({
         jobLocation: getUserFromLocalStorage()?.location || '',
       };
     },
+    setEditJob: (state, { payload }) => {
+      return { ...state, isEditing: true, ...payload };
+    },
   },
   extraReducers: {
     [createJob.pending]: (state) => {
@@ -78,6 +81,6 @@ const jobSlice = createSlice({
   },
 });
 
-export const { handleChange, clearValues } = jobSlice.actions;
+export const { handleChange, clearValues, setEditJob } = jobSlice.actions;
 
 export default jobSlice.reducer;
