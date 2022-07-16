d8383507035dee94e733a76d3ef3bc97a22b72d4
Shaw Lu <shawlu95@gmail.com>
2022-07-15 23:43:08 -0700

    Setup all-jobs page structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,19 @@
 import BigSidebar from './BigSidebar';
 import FormRow from './FormRow';
 import FormRowSelect from './FormRowSelect';
+import JobsContainer from './JobsContainer';
 import Logo from './Logo';
 import Navbar from './Navbar';
+import SearchContainer from './SearchContainer';
 import SmallSidebar from './SmallSidebar';
 
-export { BigSidebar, FormRow, FormRowSelect, Logo, Navbar, SmallSidebar };
+export {
+  BigSidebar,
+  FormRow,
+  FormRowSelect,
+  JobsContainer,
+  Logo,
+  Navbar,
+  SearchContainer,
+  SmallSidebar,
+};
