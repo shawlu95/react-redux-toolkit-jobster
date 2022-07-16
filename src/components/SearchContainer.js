a2200d8a276894f0314cc9518ae3ae5a7a2ed2f5
Shaw Lu <shawlu95@gmail.com>
2022-07-16 10:25:08 -0700

    Setup SearchContainer structure

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,7 +1,66 @@
 import React from 'react';
 
+import { FormRow, FormRowSelect } from '.';
+import Wrapper from '../assets/wrappers/SearchContainer';
+import { useSelector, useDispatch } from 'react-redux';
+
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
+
+  const handleSearch = (e) => {};
+
+  const handleClearFilters = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h4>search form</h4>
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='search'
+            value={search}
+            handleChange={handleSearch}
+          />
+
+          <FormRowSelect
+            labelText='status'
+            name='searchStatus'
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
+
+          <FormRowSelect
+            labelText='type'
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className='btn btn-block btn-danger'
+            disabled={isLoading}
+            onClick={handleClearFilters}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
