e6308fff678783816159be39a7ee1b3fb874cdda
Shaw Lu <shawlu95@gmail.com>
2022-07-16 01:36:14 -0700

    Refactor deleteJobThunk

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import { getUserFromLocalStorage } from '../../utils/localStorage';
-import { createJobThunk } from './jobThunk';
+import { createJobThunk, deleteJobThunk } from './jobThunk';
 import { showLoading, hideLoading, getAllJobs } from '../allJobs/allJobsSlice';
 import customFetch from '../../utils/axios';
 
@@ -22,24 +22,8 @@ export const createJob = createAsyncThunk('job/createJob', (job, thunkApi) =>
   createJobThunk(job, thunkApi)
 );
 
-export const deleteJob = createAsyncThunk(
-  'job/deleteJob',
-  async (jobId, thunkApi) => {
-    thunkApi.dispatch(showLoading());
-    try {
-      const res = await customFetch.delete(`/jobs/${jobId}`, {
-        headers: {
-          authorization: `Bearer ${thunkApi.getState().user.user.token}`,
-        },
-      });
-      // loading will stop when getAllJobs resolves
-      thunkApi.dispatch(getAllJobs());
-      return res.data.msg;
-    } catch (err) {
-      thunkApi.dispatch(hideLoading());
-      return thunkApi.rejectWithValue(err.response.data.msg);
-    }
-  }
+export const deleteJob = createAsyncThunk('job/deleteJob', (jobId, thunkApi) =>
+  deleteJobThunk(jobId, thunkApi)
 );
 
 const jobSlice = createSlice({
--- src/features/job/jobThunk.js
+++ src/features/job/jobThunk.js
@@ -1,6 +1,7 @@
 import customFetch from '../../utils/axios';
 import { logoutUser } from '../user/userSlice';
 import { clearValues } from './jobSlice';
+import { showLoading, hideLoading, getAllJobs } from '../allJobs/allJobsSlice';
 
 export const createJobThunk = async (job, thunkApi) => {
   try {
@@ -17,5 +18,22 @@ export const createJobThunk = async (job, thunkApi) => {
       return thunkApi.rejectWithValue('Unauthorized! Logging Out...');
     }
     return thunkApi.rejectWithValue(error.response.data.msg);
+  }
+};
+
+export const deleteJobThunk = async (jobId, thunkApi) => {
+  thunkApi.dispatch(showLoading());
+  try {
+    const res = await customFetch.delete(`/jobs/${jobId}`, {
+      headers: {
+        authorization: `Bearer ${thunkApi.getState().user.user.token}`,
+      },
+    });
+    // loading will stop when getAllJobs resolves
+    thunkApi.dispatch(getAllJobs());
+    return res.data.msg;
+  } catch (err) {
+    thunkApi.dispatch(hideLoading());
+    return thunkApi.rejectWithValue(err.response.data.msg);
   }
 };
