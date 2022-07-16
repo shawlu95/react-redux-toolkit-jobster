95bdd62e46f97c7dc0aff5be828cbd7f8215ba59
Shaw Lu <shawlu95@gmail.com>
2022-07-15 18:13:45 -0700

    Complete big sidebar, using CSS to hide & show

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
+import { useSelector } from 'react-redux';
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.user);
   return (
     <Wrapper>
-      <h1>BigSidebar</h1>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
